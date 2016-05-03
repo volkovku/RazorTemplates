b8c35693d0b99cb06bb180ec7782abf15e70eb98
Kirill Volkov <volkovku@gmail.com>
2016-05-03 12:12:24 +0300

    Version number was increased. Nuget package was build.

--- src/RazorTemplates.Core/Properties/AssemblyInfo.cs
+++ src/RazorTemplates.Core/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
 
 [assembly: AssemblyTitle("RazorTemplates.Core")]
 [assembly: AssemblyDescription("RazorTemplates - Core Framework")]
